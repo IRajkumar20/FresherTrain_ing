6d4f3137762bf8cfa02754f9aaa890c5b3658b41
IRajkumar20 <ilangojayaraj07@gmail.com>
2023-07-03 16:51:07 +0530

    how are u

---
+++ Day - 1/main3.py
@@ -0,0 +1,16 @@
+import os
+
+path_name = input("enter path name:")
+file_name = input("enter file name:")
+
+file1=open(file_name,"a")
+
+for i in os.listdir(path_name):
+    if i.endswith(".txt"):
+        file2=open(path_name+'\\'+i,'r')
+        file1.write(file2.read() + '\n')
+        file2.close()
+
+print("Sucessfully append")
+
+file1.close()
